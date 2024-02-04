58dd59fa85650c6ce3061ff7ae758e90ebe480e9
Arthur <arthurdewitt6@gmail.com>
2024-02-04 16:22:45 +0300

    update

--- src/main/java/com/example/calculator/CalculatorApplication.java
+++ src/main/java/com/example/calculator/CalculatorApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan("com.example.calculator.model")
-@EnableJpaRepositories("com.example.calculator.model")
+@EnableJpaRepositories("com.example.calculator.repository")
 public class CalculatorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
